ca263455f6cadbffbc98c2f72ada64c6f9a987ac
Sam Jones <sam0jones0@gmail.com>
2021-06-18 15:11:57 +0100

    Initial commit

---
+++ roulette/roulette.py
@@ -0,0 +1,80 @@
+"""
+TODO
+"""
+
+
+class Outcome:
+    """`Outcome` contains a single outcome on which a bet can be placed.
+
+    In Roulette, each spin of the wheel has a number of `Outcome objects with
+    bets that will be paid off. For example, the “1” bin has the following
+    winning Outcome instances: “1”, “Red”, “Odd”, “Low”, “Column 1”,
+    “Dozen 1-12”, “Split 1-2”, “Split 1-4”, “Street 1-2-3”, “Corner 1-2-4-5”,
+    “Five Bet”, “Line 1-2-3-4-5-6”, “00-0-1-2-3”, “Dozen 1”, “Low” and
+    “Column 1”.
+
+    All of thee above-named bets will pay off if the wheel spins a “1”. This
+    makes a Wheel and a Bin fairly complex containers of Outcome objects.
+
+    Attributes:
+        name: The name of this `Outcome`. E.g. ``1``, ``Red``, ``Even`` or ``Low``.
+        odds: The payout odds of this outcome. Most odds are stated as 1:1 or
+            17:1, we only keep the numerator (17) and assume the denominator is 1.
+    """
+
+    def __init__(self, name: str, odds: int) -> None:
+        """Sets the instance `name` and `odds` from the parameter `name` and
+        `odds`.
+        """
+        self.name = name
+        self.odds = odds
+
+    def win_amount(self, amount: float) -> float:
+        """Multiply this `Outcome`'s odds by the given ``amount``. The product is
+        returned.
+
+        Args:
+            amount: The amount being bet.
+
+        Returns:
+            The amount in winnings excluding the initial bet.
+        """
+        return self.odds * amount
+
+    def __eq__(self, other: "Outcome") -> bool:
+        """Compare the `name` attributes of `self` and ``other``.
+
+        Args:
+            other: Another `Outcome` to compare against.
+
+        Returns:
+            True if this name matches the ``other``'s name, False otherwise.
+        """
+        return self.name == other.name
+
+    def __ne__(self, other: "Outcome") -> bool:
+        """Compare the `name` attributes of `self` and ``other``.
+
+        Args:
+            other: Another `Outcome` to compare against.
+
+        Returns:
+            True if this name does not match the ``other``'s name, False otherwise.
+        """
+        return self.name != other.name
+
+    def __hash__(self) -> int:
+        """Hash value for this outcome.
+
+        Returns:
+            The hash value of the name, ``hash(self.name)``.
+        """
+        return hash(self.name)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.odds}:1"
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(name={repr(self.name)}, odds={repr(self.odds)})"
+        )
---
+++ tests/test_outcome.py
@@ -0,0 +1,21 @@
+"""
+TODO
+"""
+
+import pytest
+
+from roulette.roulette import Outcome
+
+
+def test_outcome():
+    o1 = Outcome("Red", 1)
+    o2 = Outcome("Red", 1)
+    o3 = Outcome("Black", 2)
+    assert str(o1) == "Red 1:1"
+    assert str(o3) == "Black 2:1"
+    assert repr(o2) == "Outcome(name='Red', odds=1)"
+    assert o1 == o2
+    assert o1.odds == 1
+    assert o1.name == "Red"
+    assert o1 != o3
+    assert o2 != o3
